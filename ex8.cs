e65ce6575c08f9033dab480c37df65ead5ab8ef1
DiegoGrajedaR <diego.grajeda.7e8@itb.cat>
2024-11-18 21:55:07 +0100

    S'ha afegit fins l'exercici 8

---
+++ ex8.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Aquest programa fara un canvi de valor entre el dos números que introdueixis a continuació -->");
+
+        Console.WriteLine("Introdueix valor númeric 1:");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introdueix valor númeric 2:");
+        int valor2 = int.Parse(Console.ReadLine());
+
+
+        ActualitzarValors(ref valor1, ref valor2);
+        Console.WriteLine("");
+        Console.WriteLine($"RESPOSTA: Ara el valor númeric 1 = {valor1} i valor númeric 2 = {valor2}");
+    }
+
+    public static void ActualitzarValors(ref int valor1, ref int valor2)
+    {
+        int canvi = valor1;
+        valor1 = valor2;
+        valor2 = canvi;
+    }
+}
